3f016f0e671984420148941dfe01c426bd6b3f52
FeiFei <Virus_Cui@outlook.com>
2026-09-18 14:59:41 +0800

    add AeroUltra Board

--- src/globals.h
+++ src/globals.h
@@ -74,6 +74,10 @@
 #endif
 
 #if defined(CONFIG_BOARD_AERO_PRO_UF2)
+#define SENSOR_MAGNETOMETER_AXES_ALIGNMENT -mx, my, mz
+#endif
+
+#if defined(CONFIG_BOARD_AERO_ULTRA_UF2)
 #define SENSOR_MAGNETOMETER_AXES_ALIGNMENT -mx, -my, mz
 #endif
 
